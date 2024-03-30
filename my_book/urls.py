3ff56ff9c8ace37f470501d70cf33e4934d788f6
joraboyev04 <160149521+joraboyev04@users.noreply.github.com>
2024-03-30 15:46:56 +0500

    botmessage

--- my_book/urls.py
+++ my_book/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('janr/<slug:janr_slug>/', views.janr, name='janr'),
     path('kitoblar/<slug:guruh_slug>/', views.show_group, name='group'),
     path('cat/<slug:cat_slug>/', views.cat, name='cat'),
-
+    path('register/', views.Register, name='category'),
+    path('login/', views.Login, name='category'),
+    path('yuklash/', views.Yuklash, name='category'),
+    path('logout/', views.Logout, name='category'),
+    path('send-msg/', views.SendMsg),
 ]
